0e5657bfe40252de8657568832edd7f7cf1d890f
MetinKaim <metinkaim0@gmail.com>
2024-09-22 18:27:20 +0300

    Added cinemachine virtual camera, Fixed character movement by camera

--- SoldierMovement/Assets/Game/Scripts/Controllers/PlayerMovementController.cs
+++ SoldierMovement/Assets/Game/Scripts/Controllers/PlayerMovementController.cs
@@ -30,17 +30,37 @@ namespace Scripts.Controllers
 
             AllInputs allInputs = InputSignals.Instance.onGetAllInputs.Invoke();
 
-            // Hareket girdisini al
+            // Kameranýn yönünü al
+            Vector3 cameraForward = Camera.main.transform.forward;
+            Vector3 cameraRight = Camera.main.transform.right;
 
-            Vector3 move = new(allInputs.moveValue.x, 0, allInputs.moveValue.y);
+            // Y eksenini sýfýrla çünkü yukarý/aþaðý hareket istemiyoruz
+            cameraForward.y = 0;
+            cameraRight.y = 0;
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            // Hareket girdisini kameranýn yönüne göre ayarla
+            Vector3 move = (cameraForward * allInputs.moveValue.y + cameraRight * allInputs.moveValue.x);
+
+            // Eðer iki eksende de hareket varsa, vektörü normalize et
+            if (move.magnitude > 1f)
+            {
+                move = move.normalized;
+            }
 
             characterController.Move(speed * Time.deltaTime * move);
+
+
+            // Mouse ile karakteri döndür
             transform.Rotate(mouseSensitivity * Time.deltaTime * new Vector3(0, allInputs.rotateValue.x, 0));
 
             // Yer çekimi uygula
             _velocity.y += GRAVITY * Time.deltaTime * gravityForce;
             characterController.Move(_velocity * Time.deltaTime);
         }
+
 
         public void OnJumpStarted()
         {
